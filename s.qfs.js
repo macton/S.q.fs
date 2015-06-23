741a03f9fa020c29cf34e549798bd49b2460bcb3
Mike Acton <macton@gmail.com>
2015-06-23 11:48:03 -0700

    saveUtf8

--- s.qfs.js
+++ s.qfs.js
@@ -3,7 +3,8 @@ var Q  = require('q');
 var fs = require('fs');
 
 var qfs = {
-  stat   : function( filename ) { return Q.ninvoke( fs, 'stat', filename ); },
+  stat     : function( filename ) { return Q.ninvoke( fs, 'stat', filename ); },
+  saveUtf8 : S.curry( function( filename, text ) { return Q.ninvoke( fs, 'writeFile', filename, text, 'utf8' ); } ),
 };
 
 exports = module.exports = qfs;
