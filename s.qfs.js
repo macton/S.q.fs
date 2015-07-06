ce61efb848911cf403127e610188af60002e20a7
Mike Acton <macton@gmail.com>
2015-07-06 15:42:26 -0700

    add readUtf8

--- s.qfs.js
+++ s.qfs.js
@@ -5,6 +5,7 @@ var fs = require('fs-extra');
 var qfs = {
   stat       : function( filename ) { return Q.ninvoke( fs, 'stat', filename ); },
   outputFile : S.curry( function( filename, data ) { return Q.ninvoke( fs, 'outputFile', filename, data ); } ),
+  readUtf8   : function( filename ) { return Q.ninvoke( fs, 'readFile', filename, {encoding: 'utf8'} ); },
   copy       : S.curry( function( src, dst ) { return Q.ninvoke( fs, 'copy', src, dst ); } ),
 };
 
