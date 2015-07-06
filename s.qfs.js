cd18316af64aa52df4e4337808757462ff14f3e9
Mike Acton <macton@gmail.com>
2015-07-06 15:34:25 -0700

    use node-fs-extra

--- s.qfs.js
+++ s.qfs.js
@@ -1,10 +1,11 @@
 var S  = require('s');
 var Q  = require('q');
-var fs = require('fs');
+var fs = require('fs-extra');
 
 var qfs = {
-  stat     : function( filename ) { return Q.ninvoke( fs, 'stat', filename ); },
-  saveUtf8 : S.curry( function( filename, text ) { return Q.ninvoke( fs, 'writeFile', filename, text, 'utf8' ); } ),
+  stat       : function( filename ) { return Q.ninvoke( fs, 'stat', filename ); },
+  outputFile : S.curry( function( filename, data ) { return Q.ninvoke( fs, 'outputFile', filename, data ); } ),
+  copy       : S.curry( function( src, dst ) { return Q.ninvoke( fs, 'copy', src, dst ); } ),
 };
 
 exports = module.exports = qfs;
