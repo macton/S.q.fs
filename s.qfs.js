a1541de5ff881c4db328b2fa6d34d8d310780b4e
Mike Acton <macton@gmail.com>
2015-07-07 13:59:36 -0700

    add remove

--- s.qfs.js
+++ s.qfs.js
@@ -7,6 +7,7 @@ var qfs = {
   outputFile : S.curry( function( filename, data ) { return Q.ninvoke( fs, 'outputFile', filename, data ); } ),
   readUtf8   : function( filename ) { return Q.ninvoke( fs, 'readFile', filename, {encoding: 'utf8'} ); },
   copy       : S.curry( function( src, dst ) { return Q.ninvoke( fs, 'copy', src, dst ); } ),
+  remove     : function( path ) { return Q.ninvoke( fs, 'remove', path ); },
 };
 
 exports = module.exports = qfs;
